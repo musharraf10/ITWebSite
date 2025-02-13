b37aeea1b4467f7a198631ef5280277b2b6bc09e
musharraf10 <skmusharaf00@gmail.com>
2025-02-13 21:42:42 +0530

    Updated

--- backend/src/controllers/comp.controller.js
+++ backend/src/controllers/comp.controller.js
@@ -3,26 +3,54 @@ import Company from "../models/companies.model.js";
 export const getCompanies=async(req,res)=>{
     try {
         const company=await Company.find();
+
         if(company.length===0){
             return res.status(400).json({message:"No Companies found"});
         }
         res.status(200).json(company);
-    }
-    catch(err){
-        res.status(500).json({message:err.message});
-    }
+    }catch (e) {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: `Error occurred: ${e.message}` });
+  }
 }
 
+// Creating Company here like Regisration 
 export const postCompanies = async (req, res) => {
     try {
-        const company = new Company(req.body);
-        const result = await company.save();
-        res.status(201).json({ message: "Company created successfully", company: result });
-    } catch (err) {
-        console.error("Error here:", err);
-        res.status(500).json({ error: "Failed to create company", details: err.message });
+        const { name, address, contactEmail, thirdPartyUserId, status } = req.body;
+
+        if (!name || !address || !contactEmail || !thirdPartyUserId || !status) {
+            return res.status(400).json({ message: 'Enter All Fields' });
+        }
+
+        // Check if the company already exists with the provided email
+        const existsCompany = await Company.findOne({ contactEmail });
+        if (existsCompany) {
+            return res.status(400).json({ message: 'Company with this contact email already exists' });
+        }
+
+        // Check if the third-party user is associated with any other company
+        const thirdPartyCompany = await Company.findOne({ thirdPartyUserId });
+        if (thirdPartyCompany) {
+            return res.status(400).json({ message: 'This thirdParty User is already registered with another company' });
+        }
+
+        const newCompany = new Company({
+            name,
+            address,
+            contactEmail,
+            thirdPartyUserId,
+            status,
+        });
+
+        await newCompany.save();
+
+        return res.status(201).json({ message: 'Company successfully created', company: newCompany });
+
+    } catch (e) {
+        res.status(500).json({ message: `Error occurred: ${e.message}` });
     }
 };
+
 export const deleteCompanies=async(req,res)=>{
     try {
         const company=await Company.findByIdAndDelete(req.params.id);
@@ -35,6 +63,8 @@ export const deleteCompanies=async(req,res)=>{
         res.status(500).json({message:err.message});
     }
 };
+
+
 export const getOneCompany = async (req, res) => {
     try {
         const company = await Company.findById(req.params.id);
